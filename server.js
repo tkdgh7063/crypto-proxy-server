e5a5b000c5f0a5828349ab191d61691caa108c92
tkdgh7063 <hoo412298@gmail.com>
2025-05-06 18:38:21 +0900

    Update server.js

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+
+const BASE_URL = `http://api.coinpaprika.com/v1`;
+
+app.get("/", async (_, res) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/coins`);
+    res.json(response.data);
+  } catch (e) {
+    res.status(500).json({ error: "Fetch failed" });
+  }
+});
+
+app.get("/:coin_id", async (req, res) => {
+  const { coin_id } = req.params;
+  try {
+    const response = await axios.get(`${BASE_URL}/${coin_id}`);
+    res.json(response.data);
+  } catch (e) {
+    res.status(500).json({ error: "Fetch failed" });
+  }
+});
+
+app.get("/tickers/:coin_id", async (req, res) => {
+  const { coin_id } = req.params;
+  try {
+    const response = await axios.get(`${BASE_URL}/tickers/${coin_id}`);
+    res.json(response.data);
+  } catch (e) {
+    res.status(500).json({ error: "Fetch failed" });
+  }
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Proxy server running on port ${PORT}`);
+});
