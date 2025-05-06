45ae15d2c6838242955e09531a13c89bf21fafa8
tkdgh7063 <hoo412298@gmail.com>
2025-05-06 19:21:43 +0900

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.get("/coins", async (_, res) => {
     res.json(response.data);
   } catch (e) {
     console.log(`coins: ${e}`);
-    res.status(500).json({ error: "Fetch failed" });
+    // res.status(500).json({ error: "Fetch failed" });
+    res.json(response.data);
   }
 });
 
@@ -28,7 +29,8 @@ app.get("/coins/:coin_id", async (req, res) => {
     res.json(response.data);
   } catch (e) {
     console.log(`${coin_id}: ${e}`);
-    res.status(500).json({ error: "Fetch failed" });
+    // res.status(500).json({ error: "Fetch failed" });
+    res.json(response.data);
   }
 });
 
@@ -39,7 +41,8 @@ app.get("/tickers/:coin_id", async (req, res) => {
     res.json(response.data);
   } catch (e) {
     console.log(`tickers ${coin_id}: ${e}`);
-    res.status(500).json({ error: "Fetch failed" });
+    // res.status(500).json({ error: "Fetch failed" });
+    res.json(response.data);
   }
 });
 
